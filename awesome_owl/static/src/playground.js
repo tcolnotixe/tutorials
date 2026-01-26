fac05404576278d77d9e791ee48e6c4a76058ee7
tcolnotixe <tcolnot@ixemelis.fr>
2026-01-26 09:50:25 +0100

    Web framework 1.1 to 1.6

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,5 +1,18 @@
-import { Component } from "@odoo/owl";
+import { Component, markup, useState } from "@odoo/owl";
+import { Card } from "./card/card";
+import { Counter } from "./counter/counter";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+    static components = { Counter, Card };
+
+    setup() {
+        this.str1 = "<div class='text-primary'>some content</div>";
+        this.str2 = markup("<div class='text-primary'>some content</div>");
+        this.sum = useState({ value: 2 })
+    }
+
+    incrementSum() {
+        this.sum.value++;
+    }
 }
