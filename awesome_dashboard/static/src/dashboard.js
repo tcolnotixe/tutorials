515e4e84c5c29775f33ad85f6ebac971e4f4c2b9
tcolnotixe <tcolnot@ixemelis.fr>
2026-01-26 14:52:36 +0100

    Web framework 2.1 and 2.2: Layout and buttons

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,8 +1,36 @@
 import { Component } from "@odoo/owl";
+import { Layout } from "@web/search/layout";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+    static components = { Layout };
+
+    setup() {
+        this.display = {
+            controlPanel: {}
+        };
+
+        this.action = useService("action");
+    }
+
+    openCustomersView() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLeadsView() {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: 'Leads',
+            target: 'current',
+            res_model: 'crm.lead',
+            views: [
+                [false, 'list'],
+                [false, 'form']
+            ]
+        });
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
