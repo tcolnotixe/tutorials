37d9bab8413d32a180661a5cabd7bfea731816ac
tcolnotixe <tcolnot@ixemelis.fr>
2026-01-26 10:23:34 +0100

    Web framework 1.7 : A todo list

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,10 +1,11 @@
 import { Component, markup, useState } from "@odoo/owl";
 import { Card } from "./card/card";
 import { Counter } from "./counter/counter";
+import { TodoList } from "./todo_list/todo_list";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter, Card };
+    static components = { Counter, Card, TodoList };
 
     setup() {
         this.str1 = "<div class='text-primary'>some content</div>";
