33ec95a7aaf33be08b1ab189b7f2a6e21ec40269
tcolnotixe <tcolnot@ixemelis.fr>
2026-01-26 11:05:08 +0100

    Web framework 1.9 and 10: Add todo input, custom autofocus hook

--- awesome_owl/static/src/todo_list/todo_list.js
+++ awesome_owl/static/src/todo_list/todo_list.js
@@ -1,15 +1,27 @@
-import { Component, useState } from "@odoo/owl";
+import { Component, useState} from "@odoo/owl";
 import { TodoItem } from "./todo_item";
+import { useAutofocus } from "../utils";
 
 export class TodoList extends Component {
     static template = "awesome_owl.todo_list";
     static components = { TodoItem };
 
     setup() {
-        this.todos = useState([
-            { id: 2, description: "write tutorial", isCompleted: true },
-            { id: 3, description: "buy milk", isCompleted: false }
-        ]);
+        this.next_id = 1;
+        this.todos = useState([]);
+        useAutofocus("new_task_input");
+    }
+
+    addTodo (ev) {
+        if (ev.keyCode === 13 && ev.target.value != "") {
+            this.todos.push({
+                id: this.next_id,
+                description: ev.target.value,
+                isCompleted: false
+            });
+            ev.target.value = "";
+            this.next_id++;
+        }
     }
 
 }
