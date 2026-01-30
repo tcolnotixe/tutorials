bf3a6a9eeff8a90b819ab5dafee4667dcd49c076
tcolnotixe <tcolnot@ixemelis.fr>
2026-01-30 16:00:28 +0100

    Web framework 2.9: Making the dashboard generic

---
+++ awesome_dashboard/static/src/dashboard/number_card/number_card.js
@@ -0,0 +1,15 @@
+import { Component } from "@odoo/owl";
+
+export class NumberCard extends Component {
+    static template = "awesome_dashboard.NumberCard";
+    static props = {
+        title: {
+            type: String,
+            optional: true
+        },
+        value: Number
+    };
+    static defaultProps = {
+        title: ""
+    };
+}
