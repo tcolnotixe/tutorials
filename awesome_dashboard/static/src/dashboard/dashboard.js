d000d40bbc70d625ceaa8c29abadf44eeddf8566
tcolnotixe <tcolnot@ixemelis.fr>
2026-01-30 17:57:03 +0100

    Web framework 2.10 : Add and remove items - configuration in localStorage

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -3,6 +3,9 @@ import { Layout } from "@web/search/layout";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { DashboardItem } from "./dashboard_item/dashboard_item";
+import { Dialog } from "@web/core/dialog/dialog";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { browser } from "@web/core/browser/browser";
 
 
 class AwesomeDashboard extends Component {
@@ -17,7 +20,9 @@ class AwesomeDashboard extends Component {
         };
 
         this.action = useService("action");
+        this.dialog = useService("dialog");
         this.statistics = useState(useService("awesome_dashboard.statistics"));
+        this.state = useState({ disabledItems: localStorage.getItem("disabledDashboardItems")?.split(',') || [] });
     }
 
     openCustomersView() {
@@ -35,6 +40,55 @@ class AwesomeDashboard extends Component {
                 [false, 'form']
             ]
         });
+    }
+
+    openConfiguration() {
+        this.dialog.add(ConfigurationDialog, {
+            items: this.items,
+            disabledItems: this.state.disabledItems,
+            onApplyConfiguration: this.applyConfiguration.bind(this)
+        });
+    }
+
+    applyConfiguration(newConfiguration) {
+        this.state.disabledItems = newConfiguration;
+    }
+}
+
+class ConfigurationDialog extends Component {
+    static template = "awesome_dashboard.ConfigurationDialog";
+    static components = { Dialog, CheckBox };
+    static props = {
+        items: Object,
+        close: Function,
+        disabledItems: Array,
+        onApplyConfiguration: Function
+    };
+
+    setup() {
+        this.items = useState(this.props.items.map((item) => {
+            return {
+                ...item,
+                enabled: !this.props.disabledItems.includes(item.id)
+            }
+        }));
+    }
+
+    applyConfiguration() {
+        const disabledItemIds = this.items
+            .filter((item) => !item.enabled)
+            .map((item) => item.id);
+
+        browser.localStorage.setItem("disabledDashboardItems", disabledItemIds);
+
+        this.props.onApplyConfiguration(disabledItemIds);
+
+        this.props.close();
+    }
+
+    onCheckboxChange (value, item) {
+        console.log(value);
+        item.enabled = value;
     }
 }
 
