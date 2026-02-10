9f4fba4b84dde8a0a18c453a0673bfa36f82f36b
tcolnotixe <tcolnot@ixemelis.fr>
2026-02-10 15:23:19 +0100

    Master Web framework 1.8: Buy click bots

--- awesome_clicker/static/src/clicker_service.js
+++ awesome_clicker/static/src/clicker_service.js
@@ -1,15 +1,42 @@
 import { registry } from "@web/core/registry";
 import { reactive } from "@odoo/owl";
 
+const clickBotPrice = 1000;
+const firstMilestoneTarget = 1000;
+
 const clicker_service = {
     start() {
-        const state = reactive({ clicks: 0 });
+        const state = reactive({
+            clicks: 0,
+            level: 0,
+            clickBots: 0,
+        });
+
+        setInterval(() => {
+            state.clicks += state.clickBots * 10
+        }, 10000);
+
+        function increment(inc) {
+            state.clicks += inc;
+
+            if (state.level < 1 && state.clicks >= firstMilestoneTarget) {
+                state.level++;
+            }
+        }
+
+        function buyClickBot() {
+            if (state.clicks < clickBotPrice) {
+                return false;
+            }
+
+            state.clicks -= clickBotPrice;
+            state.clickBots++;
+        }
 
         return {
             state,
-            increment(inc) {
-                state.clicks += inc
-            }
+            increment,
+            buyClickBot
         };
     },
 };
