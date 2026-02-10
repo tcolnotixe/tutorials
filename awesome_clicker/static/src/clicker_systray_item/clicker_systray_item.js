1228a245a3ed02af4f9a27045d84b91856eb7e6c
tcolnotixe <tcolnot@ixemelis.fr>
2026-02-10 11:45:31 +0100

    Master Web framework 1.2 : Count external clicks

--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -1,15 +1,16 @@
 import { registry } from "@web/core/registry";
-import { Component, useState } from "@odoo/owl";
+import { Component, useState, useExternalListener } from "@odoo/owl";
 
 export class ClickerSystray extends Component {
     static template = "awesome_clicker.ClickerSystray";
 
     setup() {
         this.state = useState({counter: 0});
+        useExternalListener(document.body, "click", () => this.increment(1), { capture: true });
     }
 
-    increment() {
-        this.state.counter++;
+    increment(value) {
+        this.state.counter += value;
     }
 }
 
